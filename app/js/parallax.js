6490c32172f8311e2b588f4ab5b8a07563747ca0
Frank <dduction@gmail.com>
2017-08-26 01:50:06 -0700

    add parallax to hero

--- app/js/parallax.js
+++ app/js/parallax.js
@@ -278,10 +278,9 @@ var Parallax = function () {
           helpers.accelerate(layer);
         }
 
-        layer.style.position = index ? 'absolute' : 'relative';
+        layer.style.position = 'absolute';
         layer.style.display = 'block';
-        layer.style.left = 0;
-        layer.style.top = 0;
+        
 
         var depth = helpers.data(layer, 'depth') || 0;
         this.depthsX.push(helpers.data(layer, 'depth-x') || depth);
